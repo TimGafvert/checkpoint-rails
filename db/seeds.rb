19cb8da9b3cde3bf3a95550ebca05dd50e7a2d43
Admin <Tgafvert@gmail.com>
2017-07-31 09:56:41 -0400

    bugfixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,25 +9,7 @@
 # Artist.destroy_all
 require_relative './comment_data.rb'
 require_relative './post_data.rb'
-#
-# Comment.destroy_all
-# Post.destroy_all
-#
-# comment_data = get_comment_data()
-# post_data = get_post_data()
-#
-# comment_data.each_pair do |post_content, comment|
-#   info = post_data[post_content]
-#   current_post = Post.create!({
-#     content:    info[:content],
-#     is_published:  info[:is_published]
-#   })
-#
-#     Comment.create!({
-#       body:        comment[:body],
-#     })
-#   end
-# end
+
 def get_post_data
   post_data = {
     "Post1" => {
